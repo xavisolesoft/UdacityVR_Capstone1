89862705a42bf571a16cf9fd9155b7c6f5ee9471
Xavier Solé <xavisn1@gmail.com>
2018-04-09 22:05:16 +0200

    StopPoints, Movement System and Final Cheese.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour {
+	public float movementSpeed = 2;
+	public float rotationTime = 5;
+
+	public GameObject player;
+	public GameObject startUI;
+	public GameObject restartUI;
+	public GameObject startPoint;
+	public GameObject[] showPoints;
+
+	private uint showPointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		player.transform.position = startPoint.transform.position;
+		MoveToNextPoint ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void MoveToNextPoint()
+	{
+		if (showPointIndex < showPoints.Length) {
+			Vector3 nextPoint = showPoints [showPointIndex].transform.position;
+			float distance = Vector3.Distance (player.transform.position, nextPoint);
+			iTween.MoveTo(player, 
+				iTween.Hash(
+					"position", showPoints[showPointIndex].transform.position,
+					"orienttopath", true,
+					"lookTime", rotationTime,
+					"time", distance/movementSpeed, 
+					"easetype", "linear",
+					"oncomplete", "MoveToNextPointComplete",
+					"oncompletetarget", this.gameObject
+				)
+			);
+
+			showPointIndex++;
+		}
+	}
+
+	public void MoveToNextPointComplete()
+	{
+		MoveToNextPoint ();
+	}
+}
