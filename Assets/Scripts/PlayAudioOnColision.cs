08140f3892c7aa8f241ac1c7d3ef09ccd6faec50
Xavier Solé <xavisn1@gmail.com>
2018-04-15 19:10:27 +0200

    Falling pieces audio implemented.

---
+++ Assets/Scripts/PlayAudioOnColision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnColision : MonoBehaviour {
+	public GvrAudioSource audioSource;
+
+	void OnCollisionEnter(Collision collision){
+		audioSource.Play ();
+	}
+}
